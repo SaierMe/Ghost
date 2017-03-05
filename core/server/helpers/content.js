0f15f881431f01cd1254366f6ca0a99a460abcab
Xelloss <xelloss@vip.qq.com>
2017-03-05 20:33:19 +0800

    Added <!--more--> tag

--- core/server/helpers/content.js
+++ core/server/helpers/content.js
@@ -14,10 +14,18 @@ var hbs             = require('express-hbs'),
 
 content = function (options) {
     var truncateOptions = (options || {}).hash || {};
-    truncateOptions = _.pick(truncateOptions, ['words', 'characters']);
+    truncateOptions = _.pick(truncateOptions, ['words', 'characters', 'more']);
     _.keys(truncateOptions).map(function (key) {
         truncateOptions[key] = parseInt(truncateOptions[key], 10);
     });
+
+    if (truncateOptions.hasOwnProperty('more')) {
+        var m = this.html.indexOf("<!--more-->");
+        if (m != -1) {
+            this.html = this.html.slice(0,m);
+        }
+        return new hbs.handlebars.SafeString(this.html);
+    }
 
     if (truncateOptions.hasOwnProperty('words') || truncateOptions.hasOwnProperty('characters')) {
         // Legacy function: {{content words="0"}} should return leading tags.
@@ -31,6 +39,8 @@ content = function (options) {
             downsize(this.html, truncateOptions)
         );
     }
+
+    this.html = this.html.replace(/<pre><code>/ig,'<pre><code class="language-none">').replace(/<a href=\"(\/|http|ftp)/ig,'<a target="_blank" href="$1').replace(/\[coll title="(.+?)"\]/ig,'<h3>$1<sup><a href="javascript:void(0)" class="coll-btn">展开</a></sup></h3><section class="coll-content" style="display:none;">').replace(/\[\/coll\]/ig,'</section>').replace(/<img src=/ig,'<img class="lazy" data-original=');
 
     return new hbs.handlebars.SafeString(this.html);
 };
